17912acc2800eab69d76ce441daabc05f88452ca
Nagula Anish <anish.nagula@gmail.com>
2025-04-26 19:32:14 +0530

    resume link route

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import pdf from '/Anish_Resume.pdf'
 
 export default function Resume() {
   return (
     <div className="resume-container">
       <h2 className="resume-heading">🧾 My Resume</h2>
       <iframe
-        src="../../public/Anish_Resume.pdf"
+        src={pdf}
         title="Anish Nagula Resume"
         className="resume-frame"
         frameBorder="0"
